f6af015367392d39e0ecae808c63bf5d0987ee73
Neo Yin <neo@Neos-MacBook-Pro.local.ucsf.edu>
2023-08-10 01:27:58 -0700

    added annotations reading utilities

---
+++ scripts/get_PB_annotations.py
@@ -0,0 +1,30 @@
+####################################################################################################
+# Imports ##########################################################################################
+####################################################################################################
+
+# Outside imports ##################################################################################
+import os
+import pandas as pd
+from tqdm import tqdm
+
+# Within package imports ###########################################################################
+from WCW.brain.read_annotations import get_PB_annotations_from_csv, get_PB_metadata
+
+H23_csv_path = "/media/ssd2/clinical_text_data/PathReports_Heme/H23-20230720.csv"
+WSI_dir = "/pesgisipth/NDPI"
+save_dir = "/home/greg/Documents/neo"
+
+# Get the dataframe of PB annotations
+PB_annotations_df = get_PB_annotations_from_csv(H23_csv_path)
+
+# Get a list of files in WSI_dir that end with .ndpi and start with H23
+fnames = [fname for fname in os.listdir(WSI_dir) if fname.endswith(
+    ".ndpi") and fname.startswith("H23")]
+
+# the metadata are dictionaries, get them and concatenate them into a pandas dataframe
+# use tqdm to show a progress bar
+metadata_df = pd.concat([pd.DataFrame(get_PB_metadata(
+    fname, PB_annotations_df), index=[0]) for fname in tqdm(fnames)])
+
+# save the dataframe as a csv file in the save_dir with file name H23_PB_metadata.csv
+metadata_df.to_csv(os.path.join(save_dir, "H23_PB_metadata.csv"), index=False)
