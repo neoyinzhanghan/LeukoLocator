86e8c9ae15ab611f441338abf88c522c67ef0e07
Neo Yin <neo@Neos-MacBook-Pro.local.ucsf.edu>
2023-08-13 12:58:46 -0700

    added in verbosity handling

--- scripts/get_some_cells.py
+++ scripts/get_some_cells.py
@@ -3,10 +3,15 @@ import random
 import shutil
 
 sorted_folder = "/media/hdd3/neo/dump/wbc_candidates_sorted"
-save_dir = "/media/hdd3/neo/dump/"
+save_dir = "/media/hdd3/neo/dump/wbc_candidates_sorted_sampled"
 
 # for each subdirectory in sorted_folder, create a subdirectory in save_dir of the same name
 # then copy a random 100 images from sorted_folder to the new subdirectory
+
+# if save_dir does not exist, create it
+if not os.path.exists(save_dir):
+    os.mkdir(save_dir)
+
 for subdir in os.listdir(sorted_folder):
     os.mkdir(os.path.join(save_dir, subdir))
     for file in random.sample(os.listdir(os.path.join(sorted_folder, subdir)), min(100, len(os.listdir(os.path.join(sorted_folder, subdir))))):
