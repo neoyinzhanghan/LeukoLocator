c0f2cc3bf73c2ff9bf304ce30dcef499a7d0d464
Neo Yin <neo@Neos-MacBook-Pro.local>
2023-08-16 20:44:53 -0700

    new script

--- scripts/copy_PB_slides.py
+++ scripts/copy_PB_slides.py
@@ -10,10 +10,6 @@ PB_annotations_path = "/media/hdd3/neo/results/PB_annotations_filtered.csv"
 # first open the csv file in PB_annotations_path and read it into a pandas dataframe
 PB_annotations_df = pd.read_csv(PB_annotations_path)
 
-# traverse through the wsi_fname column in the dataframe, and move the files from from_dir to to_dir
+# traverse through the wsi_fname column in the dataframe, and copy the files from from_dir to to_dir
 for wsi_fname in tqdm(PB_annotations_df['wsi_fname']):
-    try:
-        shutil.move(os.path.join(from_dir, wsi_fname), os.path.join(to_dir, wsi_fname))
-    except FileNotFoundError:
-        print(f"{wsi_fname} not found")
-        continue
+    shutil.copy(os.path.join(from_dir, wsi_fname), os.path.join(to_dir, wsi_fname))
