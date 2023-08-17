c87ba03c3637e8cda0be61d9affdc19679fc4eb8
Neo Yin <neo@Neos-MacBook-Pro.local>
2023-08-16 19:10:01 -0700

    get all PB annotations script

--- WCW/resources/assumptions.py
+++ WCW/resources/assumptions.py
@@ -6,15 +6,15 @@ num_classes = 23
 top_view_patch_size = 64
 min_specimen_prop = 0.25
 foci_sds = 6
-min_VoL = 10 # 10
-min_WMP = 0.55 # 0.55
-max_WMP = 0.9 # 0.9
+min_VoL = 10  # 10
+min_WMP = 0.55  # 0.55
+max_WMP = 0.9  # 0.9
 focus_region_outlier_tolerance = 1
 num_gpus = 3
 num_cpus = 18
 num_gpus_per_manager = 1
 num_cpus_per_manager = num_cpus // (num_gpus // num_gpus_per_manager)
-allowed_time = 20 # in seconds
+allowed_time = 20  # in seconds
 
 min_cell_VoL = 10
 
@@ -27,50 +27,64 @@ dump_dir = "/media/hdd3/neo/dump"
 
 cellnames = ['B1', 'B2', 'E1', 'E4', 'ER1', 'ER2', 'ER3', 'ER4', 'ER5', 'ER6',
              'L2', 'L4', 'M1', 'M2', 'M3', 'M4', 'M5', 'M6',
-             'MO2', 'PL2', 'PL3', 'U1', 'U4'] # the last element would never be indexed
+             'MO2', 'PL2', 'PL3', 'U1', 'U4']  # the last element would never be indexed
 
 ignored_cellnames = ['ER5', 'ER6']
-what_to_ignore = 'class' # 'class' or 'instance' if ignore class, then the softmax probability of ignored classes will be set to -np.inf, if ignore instance, then instances of ignored classes will be removed
+what_to_ignore = 'class'  # 'class' or 'instance' if ignore class, then the softmax probability of ignored classes will be set to -np.inf, if ignore instance, then instances of ignored classes will be removed
 
 cellnames_dict = {
-    'M1': 'Blast',
-    'M2': 'Promyelocyte',
-    'M3': 'Myelocyte',
-    'M4': 'Metamyelocyte',
-    'M5': 'Band neutrophil',
-    'M6': 'Segmented netrophil',
-    'E0': 'Immature Eosinophil',
-    'E1': 'Eosinophil myelocyte',
-    'E2': 'Eosinophil metamyelocyte',
-    'E3': 'Eosinophil band',
-    'E4': 'Eosinophil seg',
-    'B1': 'Mast Cell',
-    'B2': 'Basophil',
-    'MO1': 'Monoblast',
-    'MO2': 'Monocyte',
-    'L0': 'Lymphoblast',
-    'L1': 'Hematogone',
-    'L2': 'Small Mature Lymphocyte',
-    'L3': 'Reactive lymphocyte/LGL',
-    'L4': 'Plasma Cell',
-    'ER1': 'Pronormoblast',
+    'M1': 'Blast',  # K
+    'M2': 'Promyelocyte',  # K, combine with blass
+    'M3': 'Myelocyte',  # K
+    'M4': 'Metamyelocyte',  # K, and combine with band and seg
+    'M5': 'Band neutrophil',  # K, and combine band and seg
+    'M6': 'Segmented netrophil',  # K, and combine band and seg
+    'E0': 'Immature Eosinophil',  # K, combine with mature eosinophil
+    'E1': 'Eosinophil myelocyte',  # K, combine with mature eosinophil
+    'E2': 'Eosinophil metamyelocyte',  # K, combine with mature eosinophil
+    'E3': 'Eosinophil band',  # K, and combine band and seg
+    'E4': 'Eosinophil seg',  # K, and combine band and seg
+    'B1': 'Mast Cell',  # K, put them with basophils
+    'B2': 'Basophil',  # K
+    'MO1': 'Monoblast',  # NA
+    'MO2': 'Monocyte',  # K
+    'L0': 'Lymphoblast',  # NA
+    'L1': 'Hematogone',  # NA
+    'L2': 'Small Mature Lymphocyte',  # K
+    'L3': 'Reactive lymphocyte/LGL',  # NA
+    'L4': 'Plasma Cell',  # K
+    'ER1': 'Pronormoblast',  # Move to M1
+    # K, for the differential create a new class nucleated erythroid
     'ER2': 'Basophilic Normoblast',
+    # K, for the differential create a new class nucleated erythroid
     'ER3': 'Polychromatophilic Normoblast',
+    # K, for the differential create a new class nucleated erythroid
     'ER4': 'Orthochromic Normoblast',
-    'ER5': 'Polychromatophilic Erythrocyte',
-    'ER6': 'Mature Erythrocyte',
-    'U1': 'Artifact',
-    'U2': 'Unknown',
-    'U3': 'Other',
-    'U4': 'Mitotic Body',
-    'U5': 'Karyorrhexis',
-    'UL': 'Unlabelled',
-    'PL1': 'Immature Megakaryocyte',
-    'PL2': 'Mature Megakaryocyte',
-    'PL3': 'Platelet Clump',
-    'PL4': 'Giant Platelet',
-    'R': 'Removed Due to Class Ignoring'
+    'ER5': 'Polychromatophilic Erythrocyte',  # M
+    'ER6': 'Mature Erythrocyte',  # M
+    'U1': 'Artifact',  # R
+    'U2': 'Unknown',  # R
+    'U3': 'Other',  # R
+    'U4': 'Mitotic Body',  # M
+    'U5': 'Karyorrhexis',  # R
+    'UL': 'Unlabelled',  # R
+    'PL1': 'Immature Megakaryocyte',  # R
+    'PL2': 'Mature Megakaryocyte',  # R
+    'PL3': 'Platelet Clump',  # R
+    'PL4': 'Giant Platelet',  # R
+    'R': 'Removed'
 }
 
 supported_extensions = ['.svs', '.ndpi']
 test_example_path = "/media/hdd1/neo/PB2/666 - 2023-05-31 22.53.12.ndpi"
+
+different_dict = {
+    'Immature Granulocyte': ['M3'],
+    'Neutrophil': ['M4', 'M5', 'M6'],
+    'Eosinophil': ['E0', 'E1', 'E2', 'E3', 'E4'],
+    'Blast': ['M1', 'ER1', 'M2'],
+    'Monocyte': ['MO2'],
+    'Lymphocyte': ['L2'],
+    'Nucleated RBC': ['ER2', 'ER3', 'ER4'],
+    'Basophil': ['B2', 'B1'],
+}
