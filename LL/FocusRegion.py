dea22fd7382fb14480704d9da2d5f416e9374119
Neo Yin <yinzhang@berkeley.edu>
2023-11-01 22:38:00 -0700

    major reconstruction of pre-NN processings

--- LL/FocusRegion.py
+++ LL/FocusRegion.py
@@ -182,7 +182,6 @@ def predict(pil_image, model):
         [
             transforms.Resize(256),
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ]
     )
     image = pil_image.convert("RGB")
