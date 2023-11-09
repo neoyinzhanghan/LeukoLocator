4a9bc4b9bad893e92f75584804797897c279a77b
Neo Yin <yinzhang@berkeley.edu>
2023-11-08 19:33:44 -0800

    testing YOLO df

--- LL/PBCounter.py
+++ LL/PBCounter.py
@@ -215,9 +215,6 @@ class PBCounter:
         if self.verbose:
             print(f"Initializing {num_croppers} Ray workers")
 
-        if self.verbose:
-            print(f"Initializing {num_gpus} Ray workers")
-
         ray.shutdown()
         ray.init(num_cpus=num_cpus)
 
@@ -225,14 +222,14 @@ class PBCounter:
             print("Initializing WSICropManager")
         crop_managers = [
             WSICropManager.remote(self.file_name_manager.wsi_path)
-            for _ in range(num_gpus)
+            for _ in range(num_croppers)
         ]
 
         tasks = {}
         all_results = []
 
         for i, focus_region in enumerate(self.focus_regions):
-            manager = crop_managers[i % num_gpus]
+            manager = crop_managers[i % num_croppers]
             task = manager.async_get_focus_region_image.remote(focus_region)
             tasks[task] = focus_region
 
@@ -260,6 +257,7 @@ class PBCounter:
             print(f"Shutting down Ray")
         ray.shutdown()
 
+        print(f"{len(all_results)} focus regions successfully processed.")
         self.focus_regions = all_results
 
     def find_wbc_candidates(self):
--- LL/vision/WSICropManager.py
+++ LL/vision/WSICropManager.py
@@ -12,9 +12,9 @@ from LL.resources.assumptions import *
 from LL.vision.image_quality import VoL
 
 
-@ray.remote(num_cpus=num_cpus_per_manager)
+@ray.remote(num_cpus=num_cpus_per_cropper)
 class WSICropManager:
-    """ A class representing a manager that crops WSIs.
+    """A class representing a manager that crops WSIs.
 
     === Class Attributes ===
     - wsi_path : the path to the WSI
@@ -22,36 +22,36 @@ class WSICropManager:
     """
 
     def __init__(self, wsi_path) -> None:
-
         self.wsi_path = wsi_path
         self.wsi = None
 
     def open_slide(self):
-        """ Open the WSI. """
+        """Open the WSI."""
 
         self.wsi = openslide.OpenSlide(self.wsi_path)
 
     def close_slide(self):
-        """ Close the WSI. """
+        """Close the WSI."""
 
         self.wsi.close()
 
         self.wsi = None
 
     def crop(self, coords):
-        """ Crop the WSI at the lowest level of magnification. """
+        """Crop the WSI at the lowest level of magnification."""
 
         if self.wsi is None:
             self.open_slide()
 
         image = self.wsi.read_region(
-            coords, 0, (coords[2] - coords[0], coords[3] - coords[1]))
+            coords, 0, (coords[2] - coords[0], coords[3] - coords[1])
+        )
         image = image.convert("RGB")
 
         return image
 
     def async_get_focus_region_image(self, focus_region):
-        """ Update the image of the focus region. """
+        """Update the image of the focus region."""
 
         if focus_region.image is None:
             image = self.crop(focus_region.coordinate)
