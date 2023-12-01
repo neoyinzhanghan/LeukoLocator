ae6f50a2d1788d68cdc80fa753e45e447b93d020
Neo Yin <yinzhang@berkeley.edu>
2023-11-30 18:25:21 -0800

    update data extraction script

--- scripts/data_extraction.py
+++ scripts/data_extraction.py
@@ -35,6 +35,7 @@ exception_list = [
     "H19-3488;S11;MSK8 - 2023-06-27 23.12.56",
     "H19-8904;S10;MSKO - 2023-06-20 10.26.07",
     "H18-9809;S11;MSKJ - 2023-04-25 09.52.53",
+    "H18-9196;S11;MSK9 - 2023-06-21 21.36.14",
 ]
 
 # get the list of folder names in the dump_dir, these are the names of the WSIs that have been processed, the last one may or may not have been fully processed
