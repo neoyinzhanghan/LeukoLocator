ecf2924da0d9f97b5bf23fc3e295ac4a377759fe
Neo Yin <yinzhang@berkeley.edu>
2023-12-04 14:15:58 -0800

    updated data extraction

--- LL/brain/TextParser.py
+++ LL/brain/TextParser.py
@@ -5,13 +5,16 @@ def read_and_transpose_as_df(fname):
     """Read a csv file as a dataframe."""
     df = pd.read_csv(fname)
     # if num_sds is a column name, remove the column, and store the first number as num_sds
+    process_sds = False
     if "num_sds" in df.columns:
         num_sds = int(df["num_sds"][0])
         df = df.drop(columns=["num_sds"])
+        process_sds = True
     df = df.transpose()
     df.columns = df.iloc[0]
     df = df.drop(df.index[0])
     # add the num_sds column back
-    df["num_sds"] = num_sds
+    if process_sds:
+        df["num_sds"] = num_sds
 
     return df
