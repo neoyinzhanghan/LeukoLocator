ace0cc75a20ac5942741969f8e034cdbc1314a70
Neo Yin <yinzhang@berkeley.edu>
2023-12-04 14:11:02 -0800

    updated data extraction

--- LL/brain/TextParser.py
+++ LL/brain/TextParser.py
@@ -5,5 +5,6 @@ def read_and_transpose_as_df(fname):
     """Read a csv file as a dataframe."""
     df = pd.read_csv(fname)
     df = df.transpose()
-    # df.columns = df.iloc[0]
+    df.columns = df.iloc[0]
+    df = df.drop(df.index[0])
     return df
