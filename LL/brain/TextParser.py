6289d1248b66b01382980370d48890b2b82d8e02
Neo Yin <yinzhang@berkeley.edu>
2023-12-04 14:15:08 -0800

    updated data extraction

--- LL/brain/TextParser.py
+++ LL/brain/TextParser.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 def read_and_transpose_as_df(fname):
     """Read a csv file as a dataframe."""
+    df = pd.read_csv(fname)
     # if num_sds is a column name, remove the column, and store the first number as num_sds
     if "num_sds" in df.columns:
         num_sds = int(df["num_sds"][0])
         df = df.drop(columns=["num_sds"])
-    df = pd.read_csv(fname)
     df = df.transpose()
     df.columns = df.iloc[0]
     df = df.drop(df.index[0])
