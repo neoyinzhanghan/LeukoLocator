567aad82aa55d644c83930eabc4a1b6a9b65bea1
Neo Yin <yinzhang@berkeley.edu>
2023-12-29 17:39:01 -0800

    debug utils added

--- scripts/run_on_hang_slides.py
+++ scripts/run_on_hang_slides.py
@@ -7,7 +7,7 @@ os.makedirs(dump_dir, exist_ok=True)
 wsi_dir = "/media/hdd3/neo/PB_slides"
 
 for slide_name_stem in exception_list:
-    slide_name = slide_name_stem + ".svs"
+    slide_name = slide_name_stem + ".ndpi"
     print(slide_name)
     slide_path = os.path.join(wsi_dir, slide_name)
 
