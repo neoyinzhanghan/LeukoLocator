10ee531f0d513da10bc14bcffef448b74ba3a12e
Neo Yin <yinzhang@berkeley.edu>
2024-01-02 15:16:41 -0800

    GPU parallelization debug

---
+++ scripts/extract_top_views.py
@@ -0,0 +1,41 @@
+import os
+import openslide
+from LL.vision.processing import read_with_timeout
+from Pathlib import Path
+from tqdm import tqdm
+
+wsi_dir = "/pesgisipth/NDPI"
+save_dir = "/media/hdd3/neo/topviews"
+log_dir = "/media/hdd3/neo/topviews/extract_logs"
+
+# get a list of .ndpi files in wsi_dir whose file name stem starts with H or S
+wsi_paths = [
+    os.path.join(wsi_dir, wsi_fname)
+    for wsi_fname in os.listdir(wsi_dir)
+    if Path(wsi_fname).suffix == ".ndpi" and Path(wsi_fname).stem[0] in ["H", "S"]
+]
+
+
+def extract_top_view(wsi_path):
+    stem = Path(wsi_path).stem
+
+    try:
+        wsi = openslide.OpenSlide(wsi_path)
+
+        top_level = len(wsi.level_dimensions) - 2
+
+        top_view = read_with_timeout(
+            wsi, (0, 0), top_level, wsi.level_dimensions[top_level]
+        )
+
+        # save the topview as a jpg file in the save_dir using the same filename stem as the wsi
+        top_view.save(os.path.join(save_dir, stem + ".jpg"))
+
+    except Exception as e:
+        # write the error as a txt file in log_dir using the same filename stem as the wsi
+        with open(os.path.join(log_dir, stem + ".txt"), "w") as f:
+            f.write(str(e))
+
+
+for wsi_path in tqdm(wsi_paths, desc="Extracting top views"):
+    extract_top_view(wsi_path)
