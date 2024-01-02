856776193dc8538bdd7ebc54b13704a10ca605c3
Neo Yin <yinzhang@berkeley.edu>
2024-01-02 15:27:26 -0800

    GPU parallelization debug

--- scripts/extract_top_views.py
+++ scripts/extract_top_views.py
@@ -20,16 +20,27 @@ def extract_top_view(wsi_path):
     stem = Path(wsi_path).stem
 
     try:
+        print("Processing: " + wsi_path)
+        print("Opening slide ... ")
         wsi = openslide.OpenSlide(wsi_path)
+        print("Finished opening slide ... ")
 
         top_level = len(wsi.level_dimensions) - 1
 
+        print("Extracting view ... ")
         top_view = read_with_timeout(
             wsi, (0, 0), top_level, wsi.level_dimensions[top_level]
         )
+        print("Finished extracting view ... ")
 
+        print("Saving topview ...")
         # save the topview as a jpg file in the save_dir using the same filename stem as the wsi
         top_view.save(os.path.join(save_dir, stem + ".jpg"))
+        print("Finished saving topview ...")
+
+        print("Closing slide ... ")
+        wsi.close()
+        print("Finished closing slide ... ")
 
     except Exception as e:
         # write the error as a txt file in log_dir using the same filename stem as the wsi
