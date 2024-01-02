9e5898ce303217c0508713246bb09281fd8d4d18
Neo Yin <yinzhang@berkeley.edu>
2024-01-02 15:55:44 -0800

    GPU parallelization debug

--- scripts/extract_top_views.py
+++ scripts/extract_top_views.py
@@ -1,5 +1,6 @@
 import os
 import openslide
+import ray
 from LL.vision.processing import read_with_timeout
 from pathlib import Path
 from tqdm import tqdm
@@ -18,7 +19,8 @@ wsi_paths = [
 ]
 
 
-def extract_top_view(wsi_path):
+@ray.remote
+def extract_top_view(wsi_path, save_dir=save_dir, log_dir=log_dir):
     stem = Path(wsi_path).stem
 
     try:
@@ -39,3 +41,23 @@ def extract_top_view(wsi_path):
 
 for wsi_path in tqdm(wsi_paths, desc="Extracting top views"):
     extract_top_view(wsi_path)
+
+ray.init()
+
+tasks = []
+for i, wsi_path in enumerate(wsi_paths):
+    task = extract_top_view.remote(wsi_path, save_dir, log_dir)
+    tasks.append(task)
+
+# Initialize tqdm
+pbar = tqdm(total=len(tasks), desc="Collecting Top Views")
+
+# Monitor task completion and update tqdm
+while tasks:
+    # Wait for any one of the batch of tasks to finish
+    done_ids, tasks = ray.wait(tasks)
+    # Update the tqdm bar
+    pbar.update(len(done_ids))
+
+pbar.close()
+ray.shutdown()
