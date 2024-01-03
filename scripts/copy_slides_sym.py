f8646968838c01c57dcf8ecb25450a3eede64cf0
Neo Yin <yinzhang@berkeley.edu>
2024-01-02 16:32:35 -0800

    GPU parallelization debug

--- scripts/copy_slides_sym.py
+++ scripts/copy_slides_sym.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from tqdm import tqdm
 
 # symbolically copy slides from one directory to another
 wsi_dir = "/pesgisipth/NDPI"
@@ -8,7 +9,7 @@ save_dir = "/media/hdd3/neo/AllSlides"
 os.makedirs(save_dir, exist_ok=True)
 
 # for each file in wsi_dir that ends with .ndpi and starts with H or S,
-for wsi_fname in os.listdir(wsi_dir):
+for wsi_fname in tqdm(os.listdir(wsi_dir)):
     if Path(wsi_fname).suffix == ".ndpi" and Path(wsi_fname).stem[0] in ["H", "S"]:
         # sudo copy and duplicate the file in save_dir, DO NOT use symbolic link
         os.system(f"sudo cp -n \"{os.path.join(wsi_dir, wsi_fname)}\" {save_dir}")
