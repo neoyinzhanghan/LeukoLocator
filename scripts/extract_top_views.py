04487330113b29633c46c3476e524cb2976e38fb
Neo Yin <yinzhang@berkeley.edu>
2024-01-02 19:31:41 -0800

    GPU parallelization debug

--- scripts/extract_top_views.py
+++ scripts/extract_top_views.py
@@ -6,6 +6,7 @@ from pathlib import Path
 
 wsi_dir = "/pesgisipth/NDPI"
 save_dir = "/media/hdd3/neo/topviews"
+tmp_dir = "/media/hdd3/neo/tmp"
 log_dir = "/media/hdd3/neo/topviews/extract_logs"
 
 # get a list of .ndpi files in wsi_dir whose file name stem starts with H or S
@@ -20,11 +21,20 @@ def extract_top_view(wsi_path, save_dir=save_dir, log_dir=log_dir):
     # you can get the stem by removing the last 5 characters from the file name (".ndpi")
     stem = Path(wsi_path).stem[:-5]
 
+    print("Copying wsi to tmp_dir")
+    # copy the wsi to tmp_dir
+    os.system(f'sudo cp -n "{wsi_path}" {tmp_dir}')
+
+    # tmp_dir should contain only one file, the wsi
+    assert len(os.listdir(tmp_dir)) == 1
+
+    tmp_path = os.path.join(tmp_dir, os.listdir(tmp_dir)[0])
     print(stem)
 
+    print("Extracting top view")
     try:
         # open the wsi in tmp_dir and extract the top view
-        wsi = openslide.OpenSlide(wsi_path)
+        wsi = openslide.OpenSlide(tmp_path)
         toplevel = wsi.level_count - 1
         topview = read_with_timeout(
             openslide.OpenSlide(wsi_path),
@@ -32,12 +42,16 @@ def extract_top_view(wsi_path, save_dir=save_dir, log_dir=log_dir):
             level=toplevel,
             dimensions=wsi.level_dimensions[toplevel],
         )
-        topview.save(os.path.join(save_dir, stem + ".png"))
+        topview.save(os.path.join(save_dir, stem + ".jpg"))
         wsi.close()
     except Exception as e:
         # write the error as a txt file in log_dir using the same filename stem as the wsi
         with open(os.path.join(log_dir, stem + ".txt"), "w") as f:
             f.write(str(e))
+
+    print("Removing wsi from tmp_dir")
+    # remove the wsi from tmp_dir
+    os.system(f'sudo rm "{tmp_path}"')
 
 
 for wsi_path in tqdm(wsi_paths):
