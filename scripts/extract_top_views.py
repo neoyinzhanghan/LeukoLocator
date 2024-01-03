025bb2d913f4e6a66c1a57f6fc1bc61afcec0853
Neo Yin <yinzhang@berkeley.edu>
2024-01-02 16:19:45 -0800

    GPU parallelization debug

--- scripts/extract_top_views.py
+++ scripts/extract_top_views.py
@@ -1,6 +1,5 @@
 import os
 import openslide
-import ray
 from LL.vision.processing import read_with_timeout
 from tqdm import tqdm
 from LL.PBCounter import PBCounter
@@ -30,7 +29,7 @@ def extract_top_view(wsi_path, save_dir=save_dir, log_dir=log_dir):
         # first terminal copy of the wsi to tmp_dir
         os.system(f"cp \"{wsi_path}\" {tmp_dir}")
         print('Done copying slide into tmp_dir')
-        
+
         # open the wsi in tmp_dir and extract the top view
         new_wsi_path = os.path.join(tmp_dir, os.path.basename(wsi_path))
         wsi = openslide.OpenSlide(new_wsi_path)
