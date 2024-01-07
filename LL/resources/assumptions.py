c96821724dbe696998a419db33120d2cdec4ec7e
Neo Yin <yinzhang@berkeley.edu>
2024-01-07 13:27:12 -0800

    YOLO parallelization batching and max num cells control

--- LL/resources/assumptions.py
+++ LL/resources/assumptions.py
@@ -46,7 +46,7 @@ min_num_regions_after_WMP_min_filter = 275
 min_num_regions_after_WMP_max_filter = 150
 min_num_regions_after_region_clf = 100
 max_num_regions_after_region_clf = 1000
-max_num_cells = 99
+max_num_cells = 3000
 
 
 ###########################
