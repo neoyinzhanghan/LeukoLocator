46773b8941fff46d251b6f92e63773c0b911499c
Neo Yin <yinzhang@berkeley.edu>
2024-02-21 09:04:38 -0800

    debug bma low-high mag mismatch

--- LL/vision/WSICropManager.py
+++ LL/vision/WSICropManager.py
@@ -8,9 +8,9 @@ import ray
 import pyvips
 
 # Within package imports ###########################################################################
-from LL.resources.PBassumptions import *
 from LL.vision.image_quality import VoL
 from LL.BMAFocusRegion import FocusRegion
+from LL.resources.BMAassumptions import search_view_level, search_view_downsample_rate
 
 
 # @ray.remote(num_cpus=num_cpus_per_cropper)
@@ -51,8 +51,9 @@ class WSICropManager:
             self.open_slide()
 
         image = self.wsi.read_region(
-            coords, level, (coords[2] - coords[0], coords[3] - coords[1])
+            coords, level, (coords[2] - coords[0], coords[3] - coords[1]) ## <<<< THIS SHIT IS FUCKED TODO
         )
+
         image = image.convert("RGB")
 
         return image
@@ -82,7 +83,15 @@ class WSICropManager:
 
         focus_regions = []
         for focus_region_coord in focus_region_coords:
-            image = self.crop(focus_region_coord, level=search_view_level)
+
+            level_0_coord = (
+                focus_region_coord[0] * search_view_downsample_rate,
+                focus_region_coord[1] * search_view_downsample_rate,
+                focus_region_coord[2] * search_view_downsample_rate,
+                focus_region_coord[3] * search_view_downsample_rate,
+            )
+
+            image = self.crop(level_0_coord, level=search_view_level)
 
             focus_region = FocusRegion(downsampled_coordinate=focus_region_coord, downsampled_image=image)
             focus_regions.append(focus_region)
