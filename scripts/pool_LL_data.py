a3656dcaae088d13b00df672941addbb863b7ce9
Neo Yin <yinzhang@berkeley.edu>
2024-03-01 10:20:33 -0800

    LL cell results pooling script

--- scripts/pool_LL_data.py
+++ scripts/pool_LL_data.py
@@ -13,19 +13,26 @@ os.makedirs(save_dir, exist_ok=True)
 # Initialize a DataFrame to store metadata
 metadata_df = pd.DataFrame(columns=['Symbolic_Link', 'Original_Path'])
 
+# List of image file extensions to check
+image_extensions = {'.jpg', '.jpeg', '.png', '.tif', '.tiff', '.bmp'}
+
 # Function to handle the symbolic link creation and update metadata DataFrame
 def handle_cell_images(class_dir, class_name, metadata_df):
     global link_counter  # Use a global counter for unique link names across classes
+    # Skip the "features" subfolder if it exists
+    if 'features' in os.listdir(class_dir):
+        os.listdir(class_dir).remove('features')
     for img_name in tqdm(os.listdir(class_dir), desc=f"Processing {class_name}"):
-        src_path = os.path.join(class_dir, img_name)
-        dst_dir = os.path.join(save_dir, class_name)
-        os.makedirs(dst_dir, exist_ok=True)
-        dst_path = os.path.join(dst_dir, str(link_counter))
-        os.symlink(src_path, dst_path)
-        # Update DataFrame using concat
-        new_row = pd.DataFrame({'Symbolic_Link': [dst_path], 'Original_Path': [src_path]})
-        metadata_df = pd.concat([metadata_df, new_row], ignore_index=True)
-        link_counter += 1
+        if pathlib.Path(img_name).suffix.lower() in image_extensions:
+            src_path = os.path.join(class_dir, img_name)
+            dst_dir = os.path.join(save_dir, class_name)
+            os.makedirs(dst_dir, exist_ok=True)
+            dst_path = os.path.join(dst_dir, str(link_counter))
+            os.symlink(src_path, dst_path)
+            # Update DataFrame using concat
+            new_row = pd.DataFrame({'Symbolic_Link': [dst_path], 'Original_Path': [src_path]})
+            metadata_df = pd.concat([metadata_df, new_row], ignore_index=True)
+            link_counter += 1
     return metadata_df
 
 link_counter = 1  # Start numbering symbolic links from 1
@@ -34,6 +41,9 @@ for folder_name in tqdm(os.listdir(data_dir), desc="Iterating folders"):
         cells_dir = os.path.join(data_dir, folder_name, 'cells')
         if os.path.isdir(cells_dir):
             for class_name in tqdm(os.listdir(cells_dir), desc=f"Processing cells in {folder_name}"):
+                # Check if the class_name is not "features"
+                if class_name == "features":
+                    continue
                 class_dir = os.path.join(cells_dir, class_name)
                 if os.path.isdir(class_dir):
                     metadata_df = handle_cell_images(class_dir, class_name, metadata_df)
