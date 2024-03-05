44e5d0f49136511165edf9fb7737cf458bd43feb
Neo Yin <yinzhang@berkeley.edu>
2024-03-05 09:33:34 -0800

    regions pooling script

--- LL/brain/BMARegionClfManager.py
+++ LL/brain/BMARegionClfManager.py
@@ -48,6 +48,23 @@ def load_clf_model(ckpt_path):
     return trained_model
 
 
+def load_clf_model_cpu(ckpt_path):
+    """Load the classifier model."""
+
+    # To deploy a checkpoint and use for inference
+    trained_model = ResNetModel.load_from_checkpoint(
+        ckpt_path
+    )  # , map_location=torch.device("cpu"))
+
+    # move the model to the CPU
+    trained_model.to("cpu")
+
+    # turn off the training mode
+    trained_model.eval()
+
+    return trained_model
+
+
 def predict_batch(pil_images, model):
     """
     Predict the confidence scores for a batch of PIL images.
@@ -70,6 +87,42 @@ def predict_batch(pil_images, model):
 
     # Move the batch to the GPU
     batch = batch.to("cuda")
+
+    with torch.no_grad():  # No need to compute gradients for inference
+        logits = model(batch)
+        probs = torch.softmax(logits, dim=1)
+
+        # prob shape is [44, 1, 3]
+
+        inadequate_confidence_scores = probs[:, 1].cpu().numpy()
+
+        # the adequate confidence score is the 1 - inadequate confidence score
+        adequate_confidence_scores = 1 - inadequate_confidence_scores
+
+    return adequate_confidence_scores
+
+def predict_batch_cpu(pil_images, model):
+    """
+    Predict the confidence scores for a batch of PIL images.
+
+    Parameters:
+    - pil_images (list of PIL.Image.Image): List of input PIL Image objects.
+    - model (torch.nn.Module): Trained model.
+
+    Returns:
+    - list of float: List of confidence scores for the class label `1` for each image.
+    """
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        # transforms.Normalize(mean=(0.61070228, 0.54225375, 0.65411311), std=(0.1485182, 0.1786308, 0.12817113))
+    ])
+
+    # Transform each image and stack them into a batch
+    batch = torch.stack([transform(image.convert("RGB")) for image in pil_images])
+
+    # Move the batch to the CPU
+    batch = batch.to("cpu")
 
     with torch.no_grad():  # No need to compute gradients for inference
         logits = model(batch)
