eae97c495ca1e0fdaee61101c5515176878ad0e8
Neo Yin <yinzhang@berkeley.edu>
2024-03-06 08:47:40 -0800

    new scripts

--- scripts/BMA/sort_regions_folder.py
+++ scripts/BMA/sort_regions_folder.py
@@ -4,16 +4,17 @@ from tqdm import tqdm
 
 regions_dir = "/Users/neo/Documents/Research/MODELS/results_bma_v4_regions_pooled"
 tmp_dir = "/Users/neo/Documents/Research/MODELS/sorted"
+plot_dir = "/Users/neo/Documents/Research/MODELS/plots"
 column_name = "RBI_2"
 csv_path = os.path.join(regions_dir, "image_metrics.csv")
 resnet_path = os.path.join(regions_dir, "resnet_scores.csv")
 
-# open the image_metrics.csv file, create symbolic links to the images in the regions_dir, make a symbolic link of all the images in regions_dir to the tmp_dir
-# the new file name should be X_m.jpg where X is the image metric specified in column_name and m is the id of the image in the csv file
 
-# open the image_metrics.csv file, traverse though all images in regions_dir and look for the corresponding row in the csv file
-# if the image is found in the csv file, create a symbolic link to the image in the tmp_dir
-# the new file name should be X_m.jpg where X is the image metric specified in column_name and m is the id of the image in the csv file
+os.makedirs(tmp_dir, exist_ok=True)
+os.makedirs(plot_dir, exist_ok=True)
+
+# open the image_metrics.csv file, create symbolic links to the images in the regions_dir, make a symbolic link of all the images in regions_dir to the tmp_dir
+# the file name should be X_m.jpg where X is the image metric specified in column_name and m is the id of the image in the csv file
 
 # get a list of all the images in the regions_dir
 image_files = [f for f in os.listdir(regions_dir) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tif', '.tiff'))]
@@ -24,18 +25,23 @@ df = pd.read_csv(csv_path)
 # open the resnet_scores.csv file as a pandas dataframe
 df_resnet = pd.read_csv(resnet_path)
 
-# create a symbolic link of all the images in regions_dir to the tmp_dir
-for image_file in tqdm(image_files, desc="Creating symbolic links"):
-    if "ResNet" not in column_name:
-        src_path = os.path.join(regions_dir, image_file)
-
-        new_file_name = f"{df.loc[df['Image Name'] == image_file][column_name].values[0]}_{image_file}"
-        dst_path = os.path.join(tmp_dir, new_file_name)
-        os.symlink(src_path, dst_path)
+for image_file in tqdm(image_files, desc="Processing Images"):
     
-    else:
-        src_path = os.path.join(regions_dir, image_file)
+    # image name is the last part of splitting by _ 
+    image_name = image_file.split("_")[-1]
 
-        new_file_name = f"{df_resnet.loc[df_resnet['Image Name'] == image_file][column_name].values[0]}_{image_file}"
-        dst_path = os.path.join(tmp_dir, new_file_name)
-        os.symlink(src_path, dst_path)
+    # get the row of the image in the dataframe
+    row = df[df["Image Name"] == image_name]
+    resnet_row = df_resnet[df_resnet["Image Name"] == image_name]
+
+    if "ResNet" not in column_name:
+        # get the column value of the image
+        value = row[column_name].values[0]
+    else:
+        value = resnet_row[column_name].values[0]
+
+    # new image name is value + _ + image_name
+    new_image_name = f"{value}_{image_name}"
+
+    # rename the image file to the new image name
+    os.rename(os.path.join(regions_dir, image_file), os.path.join(regions_dir, new_image_name))
---
+++ scripts/BMA/subsample.py
@@ -0,0 +1,24 @@
+import os
+import random
+import shutil
+from tqdm import tqdm
+
+source_folder = "/media/hdd3/neo/results_bma_v4_regions_pooled"
+output_folder = "/media/hdd3/neo/results_bma_v4_regions_pooled_subsampled"
+num = 5000
+
+os.makedirs(output_folder, exist_ok=True)
+
+# for all the image files in the source folder, create a list of the image files
+# then randomly subsample N images from the list and copy them to the output folder
+# the output folder should contain N images
+
+# get a list of all the images in the source folder
+image_files = [f for f in os.listdir(source_folder) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tif', '.tiff'))]
+
+# randomly subsample without replacement N images from the list
+subsampled_images = random.sample(image_files, num)
+
+# copy the subsampled images to the output folder
+for image in tqdm(subsampled_images, desc="Copying Images"):
+    shutil.copy2(os.path.join(source_folder, image), os.path.join(output_folder, image))
