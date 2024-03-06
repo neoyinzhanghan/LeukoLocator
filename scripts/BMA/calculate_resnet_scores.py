aa14e03812d98698d1da66817973602c8579cb2e
Neo Yin <yinzhang@berkeley.edu>
2024-03-06 10:08:14 -0800

    ray upgrade

--- scripts/BMA/calculate_resnet_scores.py
+++ scripts/BMA/calculate_resnet_scores.py
@@ -23,16 +23,18 @@ num_resnet_model_actors = 3
 
 resnet_scores = {}
 
-ray.init()  # Initialize Ray outside the loop, adjusted for your setup
-
+# Iterate over each downsampling factor
 for n in downsampling_factors:
-    print(f"Processing with downsampling factor {n}...")
 
+    ray.shutdown()
+    ray.init()  # Adjust based on your setup
+    print(f"Processing with downsampling factor {n}...")
+    
     # Create actors for this specific downsampling factor
     resnet_model_actors = [ResNetModelActor.remote(n) for _ in range(num_resnet_model_actors)]
     
-    # Prepare your batches
-    list_of_batches = create_list_of_batches_from_list(image_files, batch_size=12)
+    # Assuming downsampling_factors influence batch creation or processing
+    list_of_batches = create_list_of_batches_from_list(image_files, batch_size=5)  # Adjust batch_size as needed
 
     tasks = {}
     
@@ -42,17 +44,26 @@ for n in downsampling_factors:
             task = actor.predict_batch.remote(batch)
             tasks[task] = batch
 
-        # Wait for all tasks to complete
         while tasks:
-            done_ids, _ = ray.wait(list(tasks.keys()))
+            done_ids, _ = ray.wait(list(tasks.keys()), num_returns=len(tasks))
+
             for done_id in done_ids:
-                results = ray.get(done_id)
-                # Process results
-                pbar.update(len(tasks[done_id]))
+                try:
+                    results = ray.get(done_id)
+                    for file_path, score in zip(tasks[done_id], results):
+                        image_name = os.path.basename(file_path)
+                        if image_name not in resnet_scores:
+                            resnet_scores[image_name] = {}
+                        resnet_scores[image_name][n] = score
+                    pbar.update(len(tasks[done_id]))
+
+                except Exception as e:  # Consider specifying the exception if possible
+                    print(f"Task for batch {tasks[done_id]} failed with error: {e}")
                 del tasks[done_id]
 
-# Optionally shutdown Ray at the end if this is the end of your script
-ray.shutdown()
+    # do not continue with the for loop until all tasks are done
+    ray.get(list(tasks.keys()))
+
 # Write the collected ResNet scores to a CSV file
 fieldnames = ['Image Name'] + [f'ResNet_Score_{n}' for n in downsampling_factors]
 with open(output_csv, 'w', newline='') as csvfile:
