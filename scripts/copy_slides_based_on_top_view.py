f8248f99441470ef9f9ea022f70ab13deb7a32c6
Neo Yin <yinzhang@berkeley.edu>
2024-03-07 09:34:53 -0800

    update

---
+++ scripts/copy_slides_based_on_top_view.py
@@ -0,0 +1,18 @@
+import os
+
+topview_dir = "/media/hdd3/neo/topviews_chosen"
+source_dir = "/media/hdd1/BMAs"
+save_dir = "/media/hdd1/BMAs_chosen"
+
+# for all the images in topview_dir, there is a corresponding file in source_dir with same name but .ndpi extension
+# copy the corresponding .ndpi file to save_dir using rsync
+
+for img_name in os.listdir(topview_dir):
+
+    # check that it is actually an image
+    if not img_name.endswith(".png"):
+        continue
+
+    src_path = os.path.join(source_dir, img_name + ".ndpi")
+    dst_path = os.path.join(save_dir, img_name + ".ndpi")
+    os.system(f'rsync -av "{src_path}" "{dst_path}"')
