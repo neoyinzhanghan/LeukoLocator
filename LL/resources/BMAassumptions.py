cd137d3e656b7470e2f0aa126ed4da1fbafc0e03
Neo Yin <yinzhang@berkeley.edu>
2024-03-15 00:46:44 -0700

    implemented feature extraction with augmentation

--- LL/resources/BMAassumptions.py
+++ LL/resources/BMAassumptions.py
@@ -3,7 +3,7 @@
 #################
 
 # dump_dir = "/media/hdd2/neo/LLResults/debug" # this one is for alpaca
-dump_dir = "/media/hdd3/neo/results_bma_chosen_2"
+dump_dir = "/media/hdd3/neo/results_bma_chosen_3"
 
 ############################
 ### WSI Image Parameters ###
