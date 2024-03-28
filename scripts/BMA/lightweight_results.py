51b7c68428cedc7e2b0566958f15ce077a8251de
Neo Yin <yinzhang@berkeley.edu>
2024-03-27 21:44:44 -0400

    100 slide extraction script

--- scripts/BMA/lightweight_results.py
+++ scripts/BMA/lightweight_results.py
@@ -1,8 +1,8 @@
 import os
 from tqdm import tqdm
 
-results_folder = "/media/hdd3/neo/results_bma_chosen"
-save_folder = "/media/hdd3/neo/results_bma_chosen_LITE"
+results_folder = "/media/hdd3/neo/results_bma_100_v1"
+save_folder = "/media/hdd3/neo/results_bma_100_v1_LITE"
 
 os.makedirs(save_folder, exist_ok=True)
 
