39c533926f7a5f714fdad7a4ccf727dedf8e8ee4
Neo Yin <yinzhang@berkeley.edu>
2024-03-29 10:49:19 -0700

    slide runner and tracker

--- LLRunner/SlideMetadata.py
+++ LLRunner/SlideMetadata.py
@@ -30,9 +30,9 @@ class SlideMetadata:
         self.accession_number = self.slide_stem.split(";")[0]
         self.recorded_specimen_type = sr.get_recorded_specimen_type(self.slide_name)
         self.predicted_specimen_type = None # classify_specimen_type(self.slide_path)
-        conf_dct = None # get_specimen_conf_dict(self.slide_path)
+        self.conf_dct = None # get_specimen_conf_dict(self.slide_path)
         self.bma_confidence = None # conf_dct["Bone Marrow Aspirate"]
         self.pb_confidence =  None # conf_dct["Peripheral Blood"]
         self.mpboribma_confidence = None #conf_dct["Manual Peripheral Blood or Inadequate Bone Marrow Aspirate"]
-        self.other_confidence = conf_dct["Others"]
+        self.other_confidence = None # conf_dct["Others"]
         self.Dx, self.sub_Dx = sst.get_dx(self.accession_number)
