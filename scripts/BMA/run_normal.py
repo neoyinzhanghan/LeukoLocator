d6c4856e62d61a92c4e8f15f317213d2d0ce7a7c
Neo Yin <yinzhang@berkeley.edu>
2024-03-30 13:52:17 -0700

    script for processing normal slides

---
+++ scripts/BMA/run_normal.py
@@ -0,0 +1,66 @@
+import os
+import random
+from LLRunner.SlideTracker import SlidePoolMetadataTracker
+from tqdm import tqdm
+from LL.resources.BMAassumptions import dump_dir
+from LL.BMACounter import BMACounter
+
+# get a list of all directories in the dump_dir
+dump_dirs = [
+    dname
+    for dname in os.listdir(dump_dir)
+    if os.path.isdir(os.path.join(dump_dir, dname))
+]
+
+
+def already_processed(fname, dump_dirs):
+    for dump_dir in dump_dirs:
+        if dump_dir in fname:
+            return True
+
+    return False
+
+
+slides_folder = "/media/hdd1/BMAs"
+
+# first get the paths to all the ndpi files in the slides_folder
+slide_paths = [
+    os.path.join(slides_folder, fname)
+    for fname in os.listdir(slides_folder)
+    if fname.endswith(".ndpi")
+]
+
+# get the all the slide metadata
+slide_pool_metadata_tracker = SlidePoolMetadataTracker(slide_paths)
+
+
+# what are all the slides with the diagnosis "Plasma cell myeloma" AND are predicted to be a bone marrow aspirate?
+normal_slides = slide_pool_metadata_tracker.get_slides_from_dx("Normal BMA")
+
+print("Found", len(normal_slides), "Normal slides")
+
+# randomly select 25 slides from the aml_slides
+random.seed(0)
+normal_slides = random.sample(normal_slides, 25)
+
+for slide_metadata in tqdm(normal_slides, "Processing Normal Slides: "):
+
+    bma_fname = slide_metadata.slide_name
+
+    if already_processed(bma_fname, dump_dirs):
+        print("Already processed", bma_fname)
+        continue
+
+    print("Processing", bma_fname)
+
+    # try:
+    bma_slide_path = os.path.join(slides_folder, bma_fname)
+    bma_counter = BMACounter(
+        bma_slide_path,
+        hoarding=True,
+        continue_on_error=True,
+        do_extract_features=False,
+    )
+    bma_counter.tally_differential()
+
+    print("Saving to", bma_counter.save_dir)
