7e8d0532d02cf0881103d5de5f5e72d7f8b644f1
Neo Yin <yinzhang@berkeley.edu>
2024-04-14 22:24:59 -0700

    BMA LL V2

--- LL/BMACounter.py
+++ LL/BMACounter.py
@@ -1231,7 +1231,7 @@ class BMACounter:
                 print(os.path.exists(error_path))
                 print(os.listdir(error_path))
                 if os.path.exists(error_path) and os.listdir(error_path):
-                    os.system(f"rm -r {error_path}")
+                    os.system(f"rm -r \'{error_path}\'")
                     print("WOOHOO! REDUNDANT ERROR FOLDER DELETED!")
                 os.rename(
                     self.save_dir,
