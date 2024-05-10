63678e81475876c7bfa9b105d72eb790730addac
Neo Yin <yinzhang@berkeley.edu>
2024-05-10 06:27:23 -0700

    binary deep skippocyte detection

--- LL/brain/BMASkippocyteDetection.py
+++ LL/brain/BMASkippocyteDetection.py
@@ -228,8 +228,7 @@ def train_model(downsample_factor):
 
 def load_model(model_path, num_classes=2, device="cpu"):
     # Load the model path based on the lightning checkpoint
-    model = ResNetModel(num_classes=num_classes)
-    model.load_from_checkpoint(checkpoint_path=model_path)
+    model = ResNetModel.load_from_checkpoint(checkpoint_path=model_path, num_classes=num_classes)
     model.eval()
     return model
 
