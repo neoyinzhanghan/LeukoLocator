b6f1f1291f2cce40230c3b9099a90d9bb907cf68
Neo Yin <yinzhang@berkeley.edu>
2024-06-20 17:12:15 -0700

    imagenet resnet feature extraction

--- scripts/MIL/check_features.py
+++ scripts/MIL/check_features.py
@@ -1,6 +1,11 @@
-root_dir = "/media/hdd3/neo/resultsv5"  # Set your root directory path here
+feature_path = "/media/hdd3/neo/resultsv5/H20-6211;S11;MSKX - 2023-06-07 00.42.05/cells/E4/features_imagenet_v3"
 
 """
 Traverse through all folders in root_dir
 """
 
+import torch
+
+tens = torch.load(feature_path)
+
+print(tens.shape)
