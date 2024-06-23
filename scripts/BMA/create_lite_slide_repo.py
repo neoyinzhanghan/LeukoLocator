3545983fd23d0084c2132b7b52d085b85a4261f0
Neo Yin <yinzhang@berkeley.edu>
2024-06-23 12:20:12 -0700

    script for clearing previous skippocytes

--- scripts/BMA/create_lite_slide_repo.py
+++ scripts/BMA/create_lite_slide_repo.py
@@ -1,10 +1,12 @@
 import os
 import numpy as np
+import shutil
 from pathlib import Path
 from tqdm import tqdm
 
 data_dir = "/media/hdd2/neo/BMA_Normal"
 save_dir = "/media/hdd2/neo/BMA_Normal_lite"
+results_dir = "/media/hdd2/neo/results_bma_normal_v3"
 num_slides = 25
 
 # make the save_dir if it does not exist
@@ -13,9 +15,37 @@ os.makedirs(save_dir, exist_ok=True)
 # gather the path fo all the .ndpi files in the data_dir
 ndpi_files = list(Path(data_dir).rglob("*.ndpi"))
 
-# randomly select num_slides slides
-selected_slides = np.random.choice(ndpi_files, num_slides, replace=False)
+# get the name of all the folders in the results_dir
+subfolders = [
+    f.name
+    for f in os.scandir(results_dir)
+    if f.is_dir() and not f.name.startswith("ERROR_")
+]
 
-# make a symbolic link to the selected slides in the save_dir
-for slide in tqdm(selected_slides, desc="Creating Lite Slide Repo"):
-    os.symlink(slide, os.path.join(save_dir, slide.name))
+# for each of ndpi file, find the folder in subfolders that contain the file name (without the extension) of the ndpi file
+
+num_found = 0
+for ndpi_file in tqdm(ndpi_files, desc="Creating Lite Slide Repo"):
+
+    if num_found >= num_slides:
+        break
+    ndpi_file_name = ndpi_file.stem
+
+    # find the folder in subfolders that contains the ndpi_file_name
+    subfolder = next(
+        (subfolder for subfolder in subfolders if ndpi_file_name in subfolder),
+        None,
+    )
+
+    if subfolder is None:
+        continue
+
+    # if the subfolder starts with 'ERROR_', skip it
+    if subfolder.startswith("ERROR_"):
+        continue
+
+    # copy the ndpi file to the save_dir
+    os.makedirs(os.path.join(save_dir, subfolder), exist_ok=True)
+    shutil.copy(ndpi_file, os.path.join(save_dir, subfolder))
+
+    num_found += 1
