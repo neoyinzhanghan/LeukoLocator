f5356d37e1c30b6662aefc74a929ca3ffeebaf89
Neo Yin <yinzhang@berkeley.edu>
2024-06-23 12:06:12 -0700

    script for clearing previous skippocytes

---
+++ scripts/BMA/create_lite_slide_repo.py
@@ -0,0 +1,21 @@
+import os
+import numpy as np
+from pathlib import Path
+from tqdm import tqdm
+
+data_dir = "/media/hdd1/neo/BMA_AML"
+save_dir = "/media/hdd1/neo/BMA_AML_lite"
+num_slides = 25
+
+# make the save_dir if it does not exist
+os.makedirs(save_dir, exist_ok=True)
+
+# gather the path fo all the .ndpi files in the data_dir
+ndpi_files = list(Path(data_dir).rglob("*.ndpi"))
+
+# randomly select num_slides slides
+selected_slides = np.random.choice(ndpi_files, num_slides, replace=False)
+
+# make a symbolic link to the selected slides in the save_dir
+for slide in tqdm(selected_slides, desc="Creating Lite Slide Repo"):
+    os.symlink(slide, os.path.join(save_dir, slide.name))
